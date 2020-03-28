3d6e115371edb5ce68777516990c40e32faebc8c
KhaledCH23 <60884654+KhaledCH23@users.noreply.github.com>
2020-03-28 13:34:34 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use( getDateRequest = (req, res, next) => {
+let d = new Date()
+if (d.getHours() >=17 || d.getHours() <8){
+    res.status().send(500,'<center>Our office is not open now !!</center>') 
+}
+else{
+    next()
+}
+})
+
+app.use((err, req, res, next)=>{
+    console.log('an error occured!!!')
+  console.log(err)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3000,(err=>{
+    if(err) console.log(err)
+    else console.log('Server is running in port 3000')
+}))
